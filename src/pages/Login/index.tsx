39498791eb5bf5039d36fcf82c4b3b352c1ce498
meomun1231@gmail.com <meomun1231@gmai.com>
2023-03-31 08:44:28 +0700

    New

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,63 +1,61 @@
-import React from "react"
-import "./style.scss"
-import { Link } from "react-router-dom"
+import React from "react";
+import './style.scss';
+import {Link} from "react-router-dom"
+import { FaFacebookSquare, FaGoogle, FaTwitter } from "react-icons/fa";
 
-class Login extends React.Component {
-  state = {}
+class Login extends React.Component{
 
-  render() {
-    return (
-      <div className="login-signup-page">
-        <div className="login-page">
-          <div className="login-main">
-            <div className="login-title">Đăng Nhập</div>
-            <div className="frame-login">
-              <div className="from-group">
-                <label className="from-label">Tên đăng nhập hoặc Email</label>
-                <input className="from-input" type="text"></input>
-              </div>
-              <div className="from-group">
-                <label className="from-label">Mật khẩu</label>
-                <input className="from-input" type="password"></input>
-              </div>
-              <div>
-                <input
-                  type="submit"
-                  value="Bạn quên mật khẩu?"
-                  className="btn-forget-password"
-                ></input>
-              </div>
-              <div>
-                <input
-                  type="submit"
-                  value="Đăng nhập"
-                  className="btn-login"
-                ></input>
-              </div>
+    state = {
+
+    }
+    
+    render() {
+
+
+        return(
+            <div className="login-signup-page">           
+                <div className="login-page">
+                    <div className="login-main">
+                        <div className="login-title">
+                            Đăng Nhập
+                        </div>
+                        <div className="frame-login">
+                            <div className="from-group">
+                                <label className="from-label">Tên đăng nhập hoặc Email</label>
+                                <input className="from-input" type="text"></input>
+                            </div>
+                            <div className="from-group">
+                                <label className="from-label">Mật khẩu</label>
+                                <input className="from-input" type="password"></input>
+                            </div>
+                            <div>
+                                <input type="submit" value="Bạn quên mật khẩu?" className="btn-forget-password"></input>
+                            </div>
+                            <div>
+                                <input type="submit" value="Đăng nhập" className="btn-login"></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="login-main2">
+                        <div className="question1">Hoặc đăng nhập bằng:</div>
+                        <div className="group-login-social">
+                            <button className="btn-login-social">
+                                <FaFacebookSquare className="icon"></FaFacebookSquare>
+                            </button>
+                            <button className="btn-login-social">
+                                <FaGoogle className="icon"></FaGoogle>
+                            </button>
+                            <button className="btn-login-social">
+                                <FaTwitter className="icon"></FaTwitter>
+                            </button>
+                        </div>
+                        <div className="question2">Bạn chưa có tài khoản? Hãy đăng ký</div>
+                        <Link to="/signup" className="link-to-signup">Đăng ký</Link>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="login-main2">
-            <div className="question1">Hoặc đăng nhập bằng:</div>
-            <div className="group-login-social">
-              <button className="btn-login-social">
-                <i className="fa-brands fa-facebook fa-2xl"></i>
-              </button>
-              <button className="btn-login-social">
-                <i className="fa-brands fa-google fa-2xl"></i>
-              </button>
-              <button className="btn-login-social">
-                <i className="fa-brands fa-twitter fa-2xl"></i>
-              </button>
-            </div>
-            <div className="question2">Bạn chưa có tài khoản? Hãy đăng ký</div>
-            <Link to="/signup" className="link-to-signup">
-              Đăng ký
-            </Link>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Login
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,66 +1,117 @@
-import React from "react"
-import "./style.scss"
+import React from "react";
+import './style.scss';
 import { Link } from "react-router-dom"
+import { FaFacebookSquare, FaGoogle, FaTwitter } from "react-icons/fa";
 
-class Signup extends React.Component {
-  state = {}
 
-  render() {
-    return (
-      <div className="signup-signup-page">
-        <div className="signup-page">
-          <div className="signup-main">
-            <div className="signup-title">Đăng ký</div>
-            <div className="frame-signup">
-              <div className="from-group">
-                <label className="from-label">Tên đăng nhập</label>
-                <input className="from-input" type="text"></input>
-              </div>
-              <div className="from-group">
-                <label className="from-label">Email</label>
-                <input className="from-input" type="email"></input>
-              </div>
-              <div className="from-group">
-                <label className="from-label">Mật khẩu</label>
-                <input className="from-input" type="password"></input>
-              </div>
-              <div className="from-group">
-                <label className="from-label">Nhập lại mật khẩu</label>
-                <input className="from-input" type="password"></input>
-              </div>
-              <div>
-                <input
-                  type="submit"
-                  value="Đăng ký"
-                  className="btn-signup"
-                ></input>
-              </div>
+class Signup extends React.Component{
+
+    state = {
+        inputTenDangNhap: '',
+        inputEmail: '',
+        inputMatKhau: '',
+        inputNhapLaiMatKhau: ''
+    }
+    
+    handleChangeTenDangNhap = (event: any) => {
+        this.setState({
+            inputTenDangNhap: event.target.value
+        })
+    }
+    handleChangeEmail = (event: any) => {
+        this.setState({
+            inputEmail: event.target.value
+        })
+    }
+    handleChangeMatKhau = (event: any) => {
+        this.setState({
+            inputMatKhau: event.target.value
+        })
+    }
+    handleChangeNhapLaiMatKhau = (event: any) => {
+        this.setState({
+            inputNhapLaiMatKhau: event.target.value
+        })
+    }
+    handleSubmit = () => {
+        if(!this.state.inputEmail || !this.state.inputMatKhau || !this.state.inputTenDangNhap || !this.state.inputNhapLaiMatKhau){
+            alert(`Bạn chưa nhập đầy đủ thông tin`)
+            return;
+        }
+        console.log(`checkdata input: `, this.state)
+        this.setState({
+            inputTenDangNhap: '',
+            inputEmail: '',
+            inputMatKhau: '',
+            inputNhapLaiMatKhau: ''
+        })
+    }
+
+    render() {
+        return(
+            <div className="signup-signup-page">           
+                <div className="signup-page">
+                    <div className="signup-main">
+                        <div className="signup-title">
+                            Đăng ký
+                        </div>
+                        <div className="frame-signup">
+                            <div className="from-group">
+                                <label className="from-label" htmlFor="inputTenDangNhap">Tên đăng nhập</label>
+                                <input className="from-input"
+                                type="text"
+                                value={this.state.inputTenDangNhap}
+                                onChange={(event) => this.handleChangeTenDangNhap(event)}></input>
+                            </div>
+                            <div className="from-group">
+                                <label className="from-label" htmlFor="inputEmail">Email</label>
+                                <input className="from-input"
+                                type="email"
+                                value={this.state.inputEmail}
+                                onChange={(event) => this.handleChangeEmail(event)}></input>
+                            </div>
+                            <div className="from-group">
+                                <label className="from-label" htmlFor="inputMatKhau">Mật khẩu</label>
+                                <input className="from-input"
+                                type="password"
+                                value={this.state.inputMatKhau}
+                                onChange={(event) => this.handleChangeMatKhau(event)}></input>
+                            </div>
+                            <div className="from-group">
+                                <label className="from-label" htmlFor="inputNhapLaiMatKhau">Nhập lại mật khẩu</label>
+                                <input className="from-input"
+                                type="password"
+                                value={this.state.inputNhapLaiMatKhau}
+                                onChange={(event) => this.handleChangeNhapLaiMatKhau(event)}></input>
+                            </div>
+                            <div>
+                                <input type="submit"
+                                value="Đăng ký"
+                                className="btn-signup"
+                                onClick={() => this.handleSubmit()}></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="signup-main2">
+                        <div className="question1">Hoặc tạo tài khoản bằng:</div>
+                        <div className="group-signup-social">
+                            <button className="btn-signup-social">
+                                <FaFacebookSquare className="icon"></FaFacebookSquare>
+                            </button>
+                            <button className="btn-signup-social">
+                                <FaGoogle className="icon"></FaGoogle>
+                            </button>
+                            <button className="btn-signup-social">
+                                <FaTwitter className="icon"></FaTwitter>
+                            </button>
+                        </div>
+                        <div className="question2">Hãy đăng nhập nếu bạn đã có tài khoản</div>
+                        <Link to="/login" className="link-to-login">Đăng nhập</Link>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="signup-main2">
-            <div className="question1">Hoặc tạo tài khoản bằng:</div>
-            <div className="group-signup-social">
-              <button className="btn-signup-social">
-                <i className="fa-brands fa-facebook fa-2xl"></i>
-              </button>
-              <button className="btn-signup-social">
-                <i className="fa-brands fa-google fa-2xl"></i>
-              </button>
-              <button className="btn-signup-social">
-                <i className="fa-brands fa-twitter fa-2xl"></i>
-              </button>
-            </div>
-            <div className="question2">
-              Hãy đăng nhập nếu bạn đã có tài khoản
-            </div>
-            <Link to="/login" className="link-to-login">
-              Đăng nhập
-            </Link>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Signup
