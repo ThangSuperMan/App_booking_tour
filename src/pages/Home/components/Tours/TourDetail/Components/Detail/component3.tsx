2e8fc4a12b0d8f5a407c152e11f5a6348e652467
meomun1231@gmail.com <meomun1231@gmai.com>
2023-04-26 10:20:51 +0700

    hehe

---
+++ src/pages/Home/components/Tours/TourDetail/Components/Detail/component3.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./style.css"
+
+const Component3: React.FC = () => {
+
+    return(
+        <Fragment>
+            <div className="p-5 ml-4 mr-4 mt-3 bd">
+                <div>helo</div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Component3
